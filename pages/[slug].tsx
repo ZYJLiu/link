83ab6a8dfada24df0a6e692d5450859ce4981fd5
John <johnzhengyuliu@gmail.com>
2023-03-14 06:49:53 -0500

    deposit from wallet

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,48 +1,170 @@
-import { useEffect, useState } from "react"
-import { Heading, VStack, Card, CardBody, Text, Button } from "@chakra-ui/react"
-import { PublicKey } from "@solana/web3.js"
+import { useCallback, useEffect, useState } from "react"
+import {
+  Heading,
+  VStack,
+  Card,
+  CardBody,
+  Text,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Spacer,
+  HStack,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Flex,
+} from "@chakra-ui/react"
+import { ChevronDownIcon, AddIcon } from "@chakra-ui/icons"
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Transaction,
+  SystemProgram,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js"
 import axios from "axios"
+import { useWallet } from "@solana/wallet-adapter-react"
 
 export default function Link() {
-  const [isReady, setIsReady] = useState(false)
-  const [id, setId] = useState("")
-  const [url, setUrl] = useState("")
+  const { publicKey, sendTransaction, connect } = useWallet()
   const [pubKey, setPubKey] = useState<PublicKey>()
+  const [balance, setBalance] = useState<number>()
+  const connection = new Connection(clusterApiUrl("devnet"))
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [amount, setAmount] = useState(0)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.post("/api/keypair", { id })
-      console.log(response.data)
-      setPubKey(new PublicKey(response.data.publicKey))
-    }
-
-    fetchData()
-  }, [id])
-
-  useEffect(() => {
-    setIsReady(true)
+  const fetchData = useCallback(async (id: string) => {
+    const response = await axios.post("/api/keypair", { id })
+    setPubKey(new PublicKey(response.data.publicKey))
   }, [])
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const hash = window.location.hash.replace("#", "")
-      setId(hash)
-      setUrl(window.location.href)
+      const id = window.location.hash.replace("#", "")
+      fetchData(id)
     }
-  }, [isReady])
+  }, [])
+
+  const fetchBalance = async () => {
+    if (pubKey) {
+      const balance = await connection.getBalance(pubKey)
+      setBalance(balance / LAMPORTS_PER_SOL)
+      console.log(balance / LAMPORTS_PER_SOL)
+    }
+  }
+
+  async function handleClick() {
+    if (publicKey && pubKey) {
+      try {
+        console.log(amount)
+        const transaction = new Transaction().add(
+          SystemProgram.transfer({
+            fromPubkey: publicKey,
+            toPubkey: pubKey,
+            lamports: amount * LAMPORTS_PER_SOL,
+          })
+        )
+        const txSig = await sendTransaction(transaction, connection)
+        console.log(txSig)
+
+        const { blockhash, lastValidBlockHeight } =
+          await connection.getLatestBlockhash()
+
+        await connection
+          .confirmTransaction(
+            {
+              blockhash,
+              lastValidBlockHeight,
+              signature: txSig,
+            },
+            "confirmed"
+          )
+          .then(() => fetchBalance())
+
+        console.log("confirmed")
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchBalance()
+  }, [connection, pubKey])
 
   return (
     <VStack justifyContent="center">
       <Heading>URL</Heading>
       <Card>
         <CardBody>
+          <HStack>
+            <Spacer />
+            <Menu placement="bottom-end">
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<ChevronDownIcon />}
+                variant="outline"
+              />
+              <MenuList>
+                <MenuItem icon={<AddIcon />} onClick={onOpen}>
+                  Deposit Assets
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </HStack>
           <VStack>
-            <Text>URL: {url}</Text>
-            <Text>Fragment Identifier: {id}</Text>
             <Text>PubKey: {pubKey?.toString()}</Text>
+            <Text>Balance: {balance}</Text>
           </VStack>
         </CardBody>
       </Card>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent width={300}>
+          <ModalHeader>Deposit</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Flex justifyContent="center">
+              <NumberInput
+                onChange={(value) => setAmount(Number(value))}
+                width={100}
+                defaultValue={0}
+                precision={2}
+                step={0.01}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter justifyContent="center">
+            <Button mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button onClick={handleClick}>Confirm Deposit</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </VStack>
   )
 }
--- pages/api/keypair.ts
+++ pages/api/keypair.ts
@@ -2,8 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import sha256 from "crypto-js/sha256"
 import { Keypair } from "@solana/web3.js"
 
-const generateKeypair = (id: string) => {
-  const buffer = Buffer.from(sha256(id).toString()).subarray(0, 32)
+const generateKeypair = (id: string, salt: string) => {
+  const saltedValue = `${id}${salt}`
+  const buffer = Buffer.from(sha256(saltedValue).toString()).subarray(0, 32)
   const keypair = Keypair.fromSeed(new Uint8Array(buffer))
   return keypair
 }
@@ -13,7 +14,8 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { id } = req.body
-  const keypair = generateKeypair(id)
+  console.log(id)
+  const keypair = generateKeypair(id, "salt")
 
   res.status(200).json({
     publicKey: keypair.publicKey.toBase58(),
